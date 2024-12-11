962baee4c966e6ec1db4cdcdba0ab85efa42b93d
Nikkihosseini <niekal00@gmail.com>
2024-12-11 11:48:33 +0330

    export addToCat to amazon.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export let cart = []
+
+export function addToCat(productId){
+    let matchingItem;
+
+    cart.forEach(cartItem => {
+        if(productId === cartItem.id){
+            matchingItem = cartItem
+        }
+    })
+
+    if(matchingItem){
+        matchingItem.quantity ++
+    }else {
+        cart.push({
+            id: productId,
+            quantity: 1
+        })
+    }
+}

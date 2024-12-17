ec4b2ccc5efcea61742d334dfb614d15cd1b6ad0
Nikkihosseini <niekal00@gmail.com>
2024-12-17 19:18:59 +0330

    Regenerat HTML when updating delivery option

--- data/cart.js
+++ data/cart.js
@@ -6,7 +6,7 @@ if(!cart){
         {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             quantity: 2,
-            deliveryOptionsId: '1'
+            deliveryOptionsId: '2'
         },
         {
             id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",

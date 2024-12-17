ed525f0cb48c69305eb8bc5019fd3e94fb0d31b5
Nikkihosseini <niekal00@gmail.com>
2024-12-17 11:39:56 +0330

    save data and generate HTML for delivery Options

--- data/cart.js
+++ data/cart.js
@@ -5,7 +5,8 @@ if(!cart){
     cart = [
         {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2
+            quantity: 2,
+            deliveryOptionsId: '1'
         }
     ]
 }
@@ -28,7 +29,8 @@ export function addToCat(productId){
     }else {
         cart.push({
             id: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionsId: '1'
         })
     }
     saveToLcalstorage()
